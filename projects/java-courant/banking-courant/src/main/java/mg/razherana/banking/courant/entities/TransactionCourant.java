a2b27f52f0efa831e8aae12e04c8195dbc0f6507
razherana <razherana@gmail.com>
2025-10-08 22:36:27 +0300

    Updated TransactionCourant to have special action field

--- projects/java-courant/banking-courant/src/main/java/mg/razherana/banking/courant/entities/TransactionCourant.java
+++ projects/java-courant/banking-courant/src/main/java/mg/razherana/banking/courant/entities/TransactionCourant.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
 package mg.razherana.banking.courant.entities;
 
 import jakarta.persistence.*;
@@ -23,70 +7,108 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "transaction_courants")
 public class TransactionCourant {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "sender_id", nullable = true)
-    private CompteCourant sender;
-    
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "receiver_id", nullable = true)
-    private CompteCourant receiver;
-    
-    @Column(nullable = false, precision = 15, scale = 2)
-    private BigDecimal montant;
-    
-    @Column(name = "date", nullable = false)
-    private LocalDateTime date = LocalDateTime.now();
+  public static enum SpecialAction {
+    DEPOSIT("deposit"), WITHDRAWAL("withdrawal"), TAXE("taxe");
 
-    public Integer getId() {
-        return id;
+    private String databaseName;
+
+    SpecialAction(String databaseName) {
+      this.databaseName = databaseName;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public String getDatabaseName() {
+      return databaseName;
+    }
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  // This field can be used to denote special actions from SpecialAction enum
+  @Column(name = "special_action", nullable = true)
+  private String specialAction;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "sender_id", nullable = true)
+  private CompteCourant sender;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "receiver_id", nullable = true)
+  private CompteCourant receiver;
+
+  @Column(nullable = false, precision = 15, scale = 2)
+  private BigDecimal montant;
+
+  @Column(name = "date", nullable = false)
+  private LocalDateTime date = LocalDateTime.now();
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public CompteCourant getSender() {
+    return sender;
+  }
+
+  public void setSender(CompteCourant sender) {
+    this.sender = sender;
+  }
+
+  public CompteCourant getReceiver() {
+    return receiver;
+  }
+
+  public void setReceiver(CompteCourant receiver) {
+    this.receiver = receiver;
+  }
+
+  public BigDecimal getMontant() {
+    return montant;
+  }
+
+  public void setMontant(BigDecimal montant) {
+    this.montant = montant;
+  }
+
+  public LocalDateTime getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDateTime date) {
+    this.date = date;
+  }
+
+  @Override
+  public String toString() {
+    return "TransactionCourant{" +
+        "id=" + id +
+        ", montant=" + montant +
+        ", date=" + date +
+        '}';
+  }
+
+  public String getSpecialAction() {
+    return specialAction;
+  }
+
+  public void setSpecialAction(String specialAction) {
+    this.specialAction = specialAction;
+  }
+
+  public SpecialAction getSpecialActionEnum() {
+    if (specialAction == null) {
+      return null;
     }
 
-    public CompteCourant getSender() {
-        return sender;
-    }
+    for (SpecialAction action : SpecialAction.values())
+      if (action.getDatabaseName().equals(specialAction))
+        return action;
 
-    public void setSender(CompteCourant sender) {
-        this.sender = sender;
-    }
-
-    public CompteCourant getReceiver() {
-        return receiver;
-    }
-
-    public void setReceiver(CompteCourant receiver) {
-        this.receiver = receiver;
-    }
-
-    public BigDecimal getMontant() {
-        return montant;
-    }
-
-    public void setMontant(BigDecimal montant) {
-        this.montant = montant;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    @Override
-    public String toString() {
-        return "TransactionCourant{" +
-                "id=" + id +
-                ", montant=" + montant +
-                ", date=" + date +
-                '}';
-    }
+    return null;
+  }
 }
